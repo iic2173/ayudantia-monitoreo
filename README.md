ef366785ccab7b8415ebbb3a5016f94ad1fc8030
Gerardo Crot Rojas <gicrot@uc.cl>
2022-11-11 12:29:02 -0300

    initial commit :tada:

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ayudantia-monitoreo
+
+## Setup
+
+Agregar los valores en el `.env` según lo que sale en `.env.example`
+
+Luego hacer `docker-compose build`
+
+Finalmente correr la aplicación con `docker-compose up`.
---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
+const app = express();
+dotenv.config(); //Reads .env file and makes it accessible via process.env
+
+const pool = new Pool({
+  host: 'db',
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
+});
+
+const connectToDB = async () => {
+  try {
+    console.log('Conectado a la DB de postgres')
+    await pool.connect();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectToDB();
+
+app.get("", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World");
+});
+
+app.get("/ping", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Pong");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at ${process.env.PORT}`);
+});
